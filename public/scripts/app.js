1451fba2f0abc2bd3c1d1b9f634e844934c3e223
Eddie Tseng <eddietseng@outlook.com>
2019-09-11 22:12:44 +0000

    Added validation to test if user tweet will exceed 140 characters or is empty.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,43 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
+//let renderedPostsArray = [] //This is where the tweets will go before being rendered.
 const renderTweets = function(data) {
   for (tweets of data) {
+    //renderedPostsArray.push(tweets)
     let $tweet = createTweetElement(tweets)
     $('#allTweets').append($tweet);
   }
-
 }
 
 const createTweetElement = function(tweet) {
-//let $tweet = $('<article>').addClass('tweetContainer');
-// ...
 return `<article class="tweetContainer">
           <header class="tweetHeader">
             <img src=${tweet.user.avatars}> 
@@ -56,39 +29,42 @@ return `<article class="tweetContainer">
           </footer>
           </article> `
 }
+
+
 $(document).ready(function(){
-  
-  //renderTweets(data);
 
   $("form#new-tweet").submit(function(e) {
     e.preventDefault()
-    $.ajax({
+    if ($("#input").val() === "" || $("#input").val() === null) {
+      alert("Please type in a tweet!")
+    } else if ($("#input").val().length > 140) {
+      alert("You have exceeded 140 Characters!") 
+    } else {
+      
+      $.ajax({
       url: "/tweets",
       type: "POST",
       data: $("form#new-tweet").serialize()
     }).then(() => {
 
-    })  
+    }
+    )
+    }
   })
 
-  
-
   const loadTweets = () => {
-      let tweets = $.ajax({
-        url: "http://localhost:8080/tweets/",
-        type: "GET",
-        dataType: 'JSON'
-      }).then((response) => {
-        renderTweets(response);
+    let tweets = $.ajax({
+      url: "http://localhost:8080/tweets/",
+      type: "GET",
+      dataType: 'JSON'
+    })
+      .then((response) => {
+      renderTweets(response);
     })
   }
 
   loadTweets();
   
-
-
-
-
 });
 
 // const renderNewTweet = async (event) => {
@@ -129,9 +105,6 @@ $(document).ready(function(){
 //     console.log("error")
 //   }
 // }
-
-
-
 
 {/* <article class="tweetContainer">
 <header class="tweetHeader">
